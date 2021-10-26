e160aa8b57802a935be1ba9ef4e31c6af145f566
Pavel <pavel@Pavel.local>
2021-10-26 17:48:52 +0300

    start_6

--- db.js
+++ db.js
@@ -3,9 +3,12 @@ module.exports = new Sequelize(
   process.env.PG_DATABASE,
   process.env.PG_USER,
   process.env.PG_PASSWORD,
+  process.env.PG_HOST,
+  process.env.PG_PORT,
   {
     dialect: "postgres",
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
+    dialectOptions: {
+      ssl: { require: true },
+    },
   }
 );
