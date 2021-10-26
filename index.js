084075fd3d2066da4a5ebd80f0a5c7c0d7f517fe
Pavel <pavel@Pavel.local>
2021-10-26 17:18:03 +0300

    start_4

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(errorHandler);
 
 const start = async () => {
   try {
-    // await sequelize.authenticate();
-    // await sequelize.sync({ force: true });
+    await sequelize.authenticate();
+    await sequelize.sync({ force: true });
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
