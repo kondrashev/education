01c2276a3be28778a38a698f3a670aa8aed9447d
Pavel <pavel@Pavel.local>
2021-10-26 17:10:30 +0300

    start_2

--- db.js
+++ db.js
@@ -1,9 +1,11 @@
 const { Sequelize } = require("sequelize");
-module.exports = new Sequelize({
-  host: "ec2-3-248-103-75.eu-west-1.compute.amazonaws.com",
-  user: "ownakmbbtmnhpk",
-  password: "fed2396be1fa5e09dae8918f6f0b6f27dcb3fb616555dcc250ccafda50045f74",
-  database: "d7btv59fvnvgm0",
-  dialect: "postgres",
-  port: 5432,
-});
+module.exports = new Sequelize(
+  process.env.PG_DATABASE,
+  process.env.PG_USER,
+  process.env.PG_PASSWORD,
+  {
+    dialect: "postgres",
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+  }
+);
