8f5566f243b9a7e751b3e26ce585feec61b47117
Pavel <pavel@Pavel.local>
2021-10-27 17:33:51 +0300

    start_8

--- db.js
+++ db.js
@@ -1,23 +1,18 @@
-// const { Sequelize } = require("sequelize");
-// module.exports = new Sequelize(
-//   process.env.PG_DATABASE,
-//   process.env.PG_USER,
-//   process.env.PG_PASSWORD,
-//   process.env.PG_HOST,
-//   process.env.PG_PORT,
-//   {
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: { require: true },
-//     },
-//   }
-// );
-const { Client } = require("pg");
-module.exports = new Client({
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-  host: process.env.PG_HOST,
-  ssl: true,
-});
+const { Sequelize } = require("sequelize");
+// module.exports = new Sequelize("education", "postgres", "1976", {
+//   host: "localhost",
+//   dialect: "postgres",
+//   port: 5433,
+//   ssl: true,
+// });
+module.exports = new Sequelize(
+  "d7btv59fvnvgm0",
+  "ownakmbbtmnhpk",
+  "fed2396be1fa5e09dae8918f6f0b6f27dcb3fb616555dcc250ccafda50045f74",
+  {
+    host: "ec2-3-248-103-75.eu-west-1.compute.amazonaws.com",
+    dialect: "postgres",
+    port: 5432,
+    ssl: true,
+  }
+);
