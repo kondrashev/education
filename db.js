03a11e0324f2f84b5b5dfba67272352c3355a9ce
Pavel <pavel@Pavel.local>
2021-10-27 19:30:28 +0300

    start_14

--- db.js
+++ db.js
@@ -7,9 +7,6 @@ const { Sequelize } = require("sequelize");
 // });
 module.exports = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  ssl: true,
-  protocol: "postgres",
-  logging: true,
   dialectOptions: {
     ssl: {
       require: true,
