593cea767db4adb5b8da113aa96454bbce960408
Pavel <pavel@Pavel.local>
2021-10-27 19:45:45 +0300

    start_15

--- db.js
+++ db.js
@@ -1,16 +1,22 @@
 const { Sequelize } = require("sequelize");
-// module.exports = new Sequelize("education", "postgres", "1976", {
-//   host: "localhost",
+//localserver
+module.exports = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+  }
+);
+//heroku
+// module.exports = new Sequelize(process.env.DATABASE_URL, {
 //   dialect: "postgres",
-//   port: 5433,
-//   ssl: true,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
 // });
-module.exports = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(errorHandler);
 const start = async () => {
   try {
     await sequelize.authenticate();
-    // await sequelize.sync({ alter: true });
-    await sequelize.sync();
+    await sequelize.sync({ alter: true });
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
