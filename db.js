72cd5bab75108683e0186f89b99f6f231628de37
Pavel <pavel@Pavel.local>
2021-10-27 19:20:37 +0300

    start_12

--- db.js
+++ db.js
@@ -5,17 +5,10 @@ const { Sequelize } = require("sequelize");
 //   port: 5433,
 //   ssl: true,
 // });
-module.exports = new Sequelize(
-  "d7btv59fvnvgm0",
-  "ownakmbbtmnhpk",
-  "fed2396be1fa5e09dae8918f6f0b6f27dcb3fb616555dcc250ccafda50045f74",
-  {
-    host: "ec2-3-248-103-75.eu-west-1.compute.amazonaws.com",
-    dialect: "postgres",
-    port: 5432,
-    ssl: true,
-    dialectOptions: {
-      ssl: { require: true },
-    },
-  }
-);
+module.exports = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  ssl: true,
+  dialectOptions: {
+    ssl: { require: true },
+  },
+});
