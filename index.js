cd0cf3031671973d86fbdbfbd3dc69b2c9cc41aa
Pavel <pavel@Pavel.local>
2021-10-28 16:17:14 +0300

    mdevelop_1

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+app.use(express.static(__dirname + "/static"));
 app.use(errorHandler);
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync({ alter: true });
+    app.get("/", (req, res) => {
+      res.sendFile("index.html");
+    });
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
