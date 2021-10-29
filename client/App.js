41747cb1392c9fb03e52565cf43a0306f3f7c8f8
Pavel <pavel@Pavel.local>
2021-10-29 13:48:39 +0300

    develop_3

---
+++ client/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello my great application!!!</div>;
+};
+ReactDOM.render(<App />, document.querySelector("#app"));
