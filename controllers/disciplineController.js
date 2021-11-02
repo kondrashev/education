dc1c500f24e771330abacce76e61617a4b86fe08
Pavel <pavel@Pavel.local>
2021-11-02 13:15:43 +0200

    basic_configuration_5

--- controllers/disciplineController.js
+++ controllers/disciplineController.js
@@ -2,6 +2,7 @@ const { Discipline } = require("../models/models");
 const ApiError = require("../error/ApiError");
 const csv = require("csvtojson");
 const path = require("path");
+const { tkm } = require("../controllers/someFunctions");
 
 class DisciplineController {
   async addDiscipline(req, res, next) {
@@ -34,119 +35,92 @@ class DisciplineController {
     return res.json();
   }
   async uploadInformation(req, res, next) {
-    const { csvFile } = req.files;
-    const { discipline, group } = req.body;
-    let fileName = "data.csv";
-    csvFile.mv(path.resolve(__dirname, "..", "static/csv", fileName));
-    let data = await csv().fromFile("static/csv/data.csv");
-    let newData = [];
-    data.forEach((item) => {
-      newData.push(Object.values(item));
-    });
-    data = newData;
-    newData = data.map((item) => {
-      return item[0].split(";");
-    });
-    data = newData;
-    newData = data.map(
-      ([
-        ,
-        a1,
-        ,
-        ,
-        a2,
-        ,
-        a3,
-        a4,
-        a5,
-        a6,
-        a7,
-        a8,
-        a9,
-        a10,
-        a11,
-        a12,
-        a13,
-        a14,
-        a15,
-        a16,
-        a17,
-        a18,
-        ...item
-      ]) => {
-        return [
+    try {
+      const { csvFile } = req.files;
+      const { discipline, group } = req.body;
+      let fileName = "data.csv";
+      csvFile.mv(path.resolve(__dirname, "..", "static/csv", fileName));
+      let data = await csv().fromFile("static/csv/data.csv");
+      let newData = [];
+      data.forEach((item) => {
+        newData.push(Object.values(item));
+      });
+      data = newData;
+      newData = data.map((item) => {
+        return item[0].split(";");
+      });
+      data = newData;
+      newData = data.map(
+        ([
+          ,
           a1,
+          ,
+          ,
           a2,
-          a3,
-          a4,
-          a5,
-          a6,
-          a7,
-          a8,
-          a9,
-          a10,
-          a11,
-          a12,
-          a13,
-          a14,
-          a15,
-          a16,
-          a17,
-          a18,
-        ];
-      }
-    );
-    data = newData;
-    newData = data.map(
-      ([
-        a1,
-        a2,
-        a3,
-        a4,
-        a5,
-        a6,
-        a7,
-        a8,
-        a9,
-        a10,
-        a11,
-        a12,
-        a13,
-        a14,
-        a15,
-        a16,
-        a17,
-        a18,
-        ...item
-      ]) => {
+          ,
+          a3 = "",
+          a4 = "",
+          a5 = "",
+          a6 = "",
+          a7 = "",
+          a8 = "",
+          a9 = "",
+          a10 = "",
+          a11 = "",
+          a12 = "",
+          a13 = "",
+          a14 = "",
+          a15 = "",
+          a16 = "",
+          a17 = "",
+          a18 = "",
+          ...item
+        ]) => {
+          return [
+            a1,
+            a2,
+            a3,
+            a4,
+            a5,
+            a6,
+            a7,
+            a8,
+            a9,
+            a10,
+            a11,
+            a12,
+            a13,
+            a14,
+            a15,
+            a16,
+            a17,
+            a18,
+          ];
+        }
+      );
+      data = newData;
+      const toChoose = (item) => {
+        switch (discipline) {
+          case "ТКМ":
+            return tkm(item);
+        }
+      };
+      newData = data.map((item = []) => {
         return {
-          surName: a1,
-          group: a2,
-          test_31: a3,
-          test_32: a4,
-          test_33: a5,
-          test_34: a6,
-          test_41: a7,
-          test_43: a8,
-          test_51: a9,
-          test_53: a10,
-          test_56: a11,
-          test_57: a12,
-          test_65: a13,
-          test_66: a14,
-          test_67: a15,
-          test_68: a16,
-          test_72: a17,
-          test_73: a18,
+          surName: item[0],
+          group: item[1],
+          options: toChoose(item),
           teacher: "",
           exercise: "",
           rating: "",
           exam: "Н/З",
         };
-      }
-    );
-    data = newData.filter((item) => item.group === group);
-    return res.json(data);
+      });
+      data = newData.filter((item) => item.group === group);
+      return res.json(data);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 }
 module.exports = new DisciplineController();
---
+++ controllers/someFunctions.js
@@ -0,0 +1,23 @@
+const tkm = (item) => {
+  return {
+    test_31: item[2],
+    test_32: item[3],
+    test_33: item[4],
+    test_34: item[5],
+    test_41: item[6],
+    test_43: item[7],
+    test_51: item[8],
+    test_53: item[9],
+    test_56: item[10],
+    test_57: item[11],
+    test_65: item[12],
+    test_66: item[13],
+    test_67: item[14],
+    test_68: item[15],
+    test_72: item[16],
+    test_73: item[17],
+  };
+};
+module.exports = {
+  tkm,
+};
